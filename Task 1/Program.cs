aeaec6f1fcc2a571c9c00034ad25c7fa74cb8aaa
Aleks Fin <finall.git2022@gmail.com>
2022-06-20 16:22:54 +0300

    Добавлены задачи к четыерному семинару

---
+++ Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 1: Напишите цикл, который принимает на вход два числа
+ (A и B) и возводит число A в натуральную степень B. */
+
+Console.Write("Введите число А: ");
+string ia = Console.ReadLine();
+int a = int.Parse(ia);
+Console.Write("Введите число B: ");
+string ib = Console.ReadLine();
+int b = int.Parse(ib);
+int i = 0;
+long p = 1;
+for (i = 1; i <= b; i++)
+{
+    p = p * a;
+}
+Console.Write($"{a} в степени {b} равно {p}");
---
+++ Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 2: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе. */
+
+Console.Write("Введите число: ");
+string istring = Console.ReadLine();
+int inum = int.Parse(istring);
+int lenght = istring.Length;
+int[] arr = new int[lenght];
+int index = 0;//lenght - 1;
+int sum = 0;
+for (index = 0; index < lenght; index++)
+{
+    arr[index] = inum % 10;
+    inum = inum / 10;
+    sum = sum + arr[index];
+    index = index++;
+}
+Console.Write($"Сумма цифр числа {istring} равна {sum}");
